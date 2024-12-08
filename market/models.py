baa76ffd0c9478d043535dafb2527a9fe45a1db1
KanatSeytov <kseytov@gmail.com>
2024-12-08 19:00:16 +0500

    add tostring for models

--- market/models.py
+++ market/models.py
@@ -14,6 +14,9 @@ class Distributor(models.Model):
     logo = models.CharField(max_length=255, null=True, blank=True) # rename to logo_url
     rating = models.FloatField(default=0)
 
+    def __str__(self) -> str:
+        return f"{self.name}"
+
 """- `id` (integer) – ID товара.
 - `sku` (string) – артикул.
 - `name` (string) – название.
@@ -32,19 +35,25 @@ class Product(models.Model):
     min_order = models.PositiveIntegerField()
     characteristics = models.JSONField()
 
-"""- `id` (integer) – ID акции.
-- `title` (string) – название акции.
-- `image` (string) – URL изображения.
-- `link` (string) – ссылка на акцию."""
+    def __str__(self) -> str:
+        return f"{self.name}"
+    
+
 class Promotion(models.Model):
     title = models.CharField(max_length=100)
     image = models.CharField(max_length=255, blank=True) # rename to image_url
     link = models.CharField(max_length=255, blank=True)
+    
+    def __str__(self) -> str:
+        return f"{self.title}"
 
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
     parent = models.ManyToManyField('self', blank=True, symmetrical=False, related_name='children')
+    
+    def __str__(self) -> str:
+        return f"{self.name}"
 
 
 class Supplier(models.Model):
@@ -53,6 +62,9 @@ class Supplier(models.Model):
     rating = models.FloatField(default=0)
     category = models.ForeignKey(Category, related_name='suppliers', on_delete=models.PROTECT)
 
+    def __str__(self) -> str:
+        return f"{self.name}"
+
 
 class ProductPrice(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='suppliers')
@@ -60,6 +72,8 @@ class ProductPrice(models.Model):
     price = models.FloatField()
     delivery_time = models.CharField(max_length=100)
 
+    def __str__(self) -> str:
+        return f"{self.product}, {self.supplier}"
 
 class Favorite(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='favorites')
@@ -68,14 +82,20 @@ class Favorite(models.Model):
 
     class Meta:
         unique_together= ('user', 'product')
+    
+    def __str__(self) -> str:
+        return f"{self.user}, {self.product}"
 
 
 class CartItem(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='cart_items')
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='cart_items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='products')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     quantity = models.PositiveIntegerField(default=1)
 
     class Meta:
         unique_together = ('user', 'product')
+
+    def __str__(self) -> str:
+        return f"{self.user}, {self.product}"
